f1efeee5a1b52e36685356c664e3dd5af8e9b562
Christian Schladetsch <Christian.Schladetsch@gmail.com>
2016-04-26 01:04:29 +1000

    Cleanup
    Former-commit-id: 04417e2da7d335d9351aafd94d7e45b7d17cbdcc
    Former-commit-id: 23737d9f74d64d8808139f5733cf5c26b7bbd367
    Former-commit-id: 81b2cf9169468b91c35b9171e42095abf2c2b8de

---
+++ Include/KAI/KAIClassBuilder.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// needed to make and register new classes
+
+#include "KAI/KAI.h"
+#include "KAI/BasePointer.h"
+#include "KAI/PropertyBase.h"
+#include "KAI/MethodBase.h"
+#include "KAI/ClassBase.h"
+#include "KAI/Memory.h"
+#include "KAI/Registry.h"
+#include "KAI/Class.h"
+#include "KAI/Pathname.h"
+#include "KAI/ClassBuilder.h"
+#include "KAI/BuiltinTypes/Void.h" 
+#include "KAI/FunctionBase.h" 
+#include "KAI/Algorithm.h" 
+#include "KAI/Function.h"
+#include "KAI/MethodBase.h" 
+#include "KAI/Method.h"
