7eb35d789d1e909742fbdf234b966b83479688f2
Christian Schladetsch <Christian.Schladetsch@gmail.com>
2016-04-28 18:38:36 +1000

    First running REPL on Visual Studio since 2009!
    Former-commit-id: 129dc8c6def2df91390464dbe8da5ce4ac539d87
    Former-commit-id: 8cf90ac9e2776e77bd1490568e771ec0051bf03a
    Former-commit-id: e320084f0f133c53dfcfef3799124d271e389e15

--- Projects/Console/Source/Main.cpp
+++ Projects/Console/Source/Main.cpp
@@ -21,31 +21,29 @@ KAI_END
 
 USING_NAMESPACE_KAI
 
+//error LNK2001 : unresolved external symbol "public: static char const * const kai::Type::TraitsBase<class kai::Array,28,39619,class kai::Array,class kai::Array &,class kai::Array const &>::_name" (? _name@?$TraitsBase@VArray@kai@@$0BM@$0JKMD@V12@AAV12@ABV12@@Type@kai@@
+
 void RunTests(Console &console);
 
 int main(int argc, char **argv)
 {
-	Object q;
-
 	Memory::StandardAllocator alloc;
-	Value<int> n;
-
 	Registry r(&alloc);
 
-	//std::vector<String> args;
-	//for (int N = 1; N < argc; ++N)
-	//	args.push_back(argv[N]);
-	//
-	//Console console(args, &alloc);
-//
-//#if defined(PROFILE)
-//	for (int n = 0; n < 10; ++n)
-//#endif
-//		RunTests(console);
-//
-//#if !defined(PROFILE)
-//	console.Run();
-//#endif
+	std::vector<String> args;
+	for (int N = 1; N < argc; ++N)
+		args.push_back(argv[N]);
+	
+	Console console(args, &alloc);
+
+#if defined(PROFILE)
+	for (int n = 0; n < 10; ++n)
+#endif
+		RunTests(console);
+
+#if !defined(PROFILE)
+	console.Run();
+#endif
 }
 
 void RunTests(Console &console)
