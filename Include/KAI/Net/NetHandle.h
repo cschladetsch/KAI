5f154139ba6eb7c974797d32999b598f6b8832af
Christian Schladetsch <Christian.Schladetsch@gmail.com>
2016-05-03 17:58:41 +1000

    Adding structure for agents, proxies and nodes.
    Former-commit-id: 5d901441125f53b1ac0de361360770ae62e23176
    Former-commit-id: e3b2b952b0d74fd67856be9d4e620f38b3dfd17e
    Former-commit-id: a39bc3f3df706bcefa31ee8aa870dda94f8ee1ab

--- Include/KAI/Net/NetHandle.h
+++ Include/KAI/Net/NetHandle.h
@@ -1,23 +1,26 @@
 #pragma once
 
+#include "KAI/Net/Net.h"
+
 KAI_NET_BEGIN
 
 struct NetHandle
 {
 	MacAddress MacAddress;
 	Handle Handle;
+	int TypeNumber;
 
 	friend bool operator==(NetHandle const &A, NetHandle const & B)
 	{
-		return A.MacAddress == B.MacAddress && A.Handle == B.Handle;
+		return A.MacAddress == B.MacAddress && A.Handle == B.Handle && A.TypeNumber == B.TypeNumber;
 	}
 };
 
 struct HashNetHandle
 {
-	size_t operator()(NetHandle const &) const
+	size_t operator()(NetHandle const &h) const
 	{
-		return 3;
+		return h.MacAddress.Hash() ^ h.Handle.GetValue() ^ h.TypeNumber;
 	}
 };
 
--- Include/KAI/Net/Node.h
+++ Include/KAI/Net/Node.h
@@ -2,20 +2,31 @@
 
 #include "KAI/Net/Net.h"
 
+#include <boost/asio.hpp>
+
 KAI_NET_BEGIN
 
 struct Node
 {
+	Node();
+
+	void Listen(int listen);
+	void Connect(IpAddress const &, int port);
+
+	Future Send(NetHandle, Object);
+	Future Receive(NetHandle, Object);
+
+private:
+	Registry _reg;
+	boost::asio::io_service _io;
+
+private:
 	typedef std::unordered_map<NetHandle, std::shared_ptr<ProxyCommon>, HashNetHandle> Proxies;
 	typedef std::unordered_map<NetHandle, std::shared_ptr<AgentCommon>, HashNetHandle> Agents;
 
 	Agents agemts;
 	Proxies proxies;
 
-	Node();
-
-	void Listen(int listen);
-	void Connect(IpAddress const &, int port);
 };
 
 KAI_NET_END
