136ecbad22720f0197fe1303a574e95df7776d93
Christian Schladetsch <Christian.Schladetsch@gmail.com>
2016-05-03 17:58:41 +1000

    Adding structure for agents, proxies and nodes.
    Former-commit-id: 5d901441125f53b1ac0de361360770ae62e23176
    Former-commit-id: e3b2b952b0d74fd67856be9d4e620f38b3dfd17e
    Former-commit-id: a39bc3f3df706bcefa31ee8aa870dda94f8ee1ab

--- Include/KAI/Net/Net.h
+++ Include/KAI/Net/Net.h
@@ -7,6 +7,7 @@ struct Connection;
 #include "KAI/Net/Base.h"
 #include "KAI/Net/Address.h"
 #include "KAI/Net/NetHandle.h"
+#include "KAI/Net/Future.h"
 #include "KAI/Net/Connection.h"
 #include "KAI/Net/Representative.h"
 #include "KAI/Net/ProxyCommon.h"
