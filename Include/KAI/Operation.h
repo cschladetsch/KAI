22113028f2388a10fbe11359b31a4bd928c1fcde
Christian Schladetsch <christian.schladetsch@gmail.com>
2016-05-14 22:06:17 +1000

    WIP: after 10 hrs, just checking in
    Former-commit-id: c11d7c077953e58b723e5914511e9284095412aa
    Former-commit-id: 8ae7fa70b62f3309d1c834f02a311ec7600327e0
    Former-commit-id: 9de7ee395a2a9da8998cba398fcd1ba0d8471996

--- Include/KAI/Operation.h
+++ Include/KAI/Operation.h
@@ -124,7 +124,6 @@ public:
 	static void Register(Registry &);
 };
 
-//KAI_TYPE_TRAITS(Operation, Number::Operation , Properties::Streaming);
 KAI_TYPE_TRAITS(Operation, Number::Operation, 0);
 
 KAI_END
