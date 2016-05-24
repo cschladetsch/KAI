d2ad08cf388547f560866d979badd723943b588e
Christian <christian@twobulls.com>
2016-05-24 20:55:37 +1000

    Added RxCpp and sample app. Added wxWidgets and sample app.
    Former-commit-id: 56c8438522f528099f8279d61bbea7190d843bca
    Former-commit-id: f51cba84950d027d1250f492d9df1639537d4c7d
    Former-commit-id: 9baa299db8741e4a962a358226fc04bf9cd989a0

--- App/WxWindow/Source/Main.cpp
+++ App/WxWindow/Source/Main.cpp
@@ -1,8 +1,34 @@
-#pragma once
-
 #include "Main.h"
+#include "Button.h"
+#include "Communicate.h"
+#include <memory>
+#include <wx/wx.h>
+#include <wx/string.h>
 
-int main(int arc, char *argv[])
+using namespace std;
+
+class Simple : public wxFrame
 {
-	return -1;
-}
+public:
+	Simple(const wxString& title)
+		: wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
+	{
+		Center();
+	}
+
+};
+
+class MyApp : public wxApp
+{
+public:
+	virtual bool OnInit() override
+	{
+		Communicate *communicate = new Communicate(wxT("Widgets communicate"));
+		communicate->Show(true);
+		return true;
+	}
+
+};
+
+IMPLEMENT_APP(MyApp)
+
