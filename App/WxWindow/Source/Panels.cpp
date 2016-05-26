2a5f182a9b811b97822b0caa239aedb6b5b76e64
Christian <christian.schladetsch@gmail.com>
2016-05-26 19:18:27 +1000

    Added more tests and cmake macros.

--- App/WxWindow/Source/Panels.cpp
+++ App/WxWindow/Source/Panels.cpp
@@ -39,7 +39,7 @@ RightPanel::RightPanel(wxPanel * parent)
 	: wxPanel(parent, wxID_ANY, wxDefaultPosition,
 	          wxSize(270, 150), wxBORDER_SUNKEN)
 {
-	//m_text = new wxStaticText(this, -1, wxT("0"), wxPoint(40, 60));
+	m_text = new wxStaticText(this, -1, wxT("0"), wxPoint(40, 60));
 }
 
 
