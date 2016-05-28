79578dd6bd1b1640362c808c8599abb2445c747b
Christian <christian.schladetsch@gmail.com>
2016-05-29 02:05:00 +1000

    Changes
    Former-commit-id: 578e0b6e2c1a9f6da0ac0c7829761394edee32c9
    Former-commit-id: b905489cfe4dcd8f5263a2cc9a00157c6f37e752
    Former-commit-id: abad2d61de3e9888aa21fc583cf6b6ace3176e61

--- Library/NetworkGen/Include/TauAgent.h
+++ Library/NetworkGen/Include/TauAgent.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "Tau/Config.h"
+#include "Tau/TauAstNode.h"
 
 TAU_BEGIN
 
+		struct GenerateProxy
+		{
+			GenerateProxy(const char * file, NodeType root);
+		};
+
 TAU_END
-
-
 
--- Library/NetworkGen/Include/TauProxy.h
+++ Library/NetworkGen/Include/TauProxy.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "Tau/Config.h"
+#include "Tau/TauAstNode.h"
 
 TAU_BEGIN
 
+struct GenerateAgent
+{
+	GenerateAgent(const char * file, NodeType root);
+};
+
 TAU_END
-
-
 
