b2a036ede899f017cf644d452cb1223305e0fe9a
Christian <christian.schladetsch@gmail.com>
2016-05-28 21:56:52 +1000

    First tokeniser test passses for Tau
    Former-commit-id: b90cd9f1c819f77d5fb9af5b3e42d8db07fbc88a
    Former-commit-id: 6bc3c0cf1417c8b68bea6c2ca31e78555a8c7e55
    Former-commit-id: 3ff80d69e9966625da58ab96ab924cfed7b29218

--- Library/NetworkGen/Source/Tau/TauLexer.cpp
+++ Library/NetworkGen/Source/Tau/TauLexer.cpp
@@ -37,49 +37,47 @@ bool TauLexer::NextToken()
 	case '(': return Add(Enum::OpenParan);
 	case ')': return Add(Enum::CloseParan);
 	case ',': return Add(Enum::Comma);
-	//case ':': return Add(Enum::Colon);
-			/*
 	case ' ': return Add(Enum::Whitespace, Gather(IsSpaceChar));
-	case '@': return Add(Enum::Lookup);
-	case '*': return Add(Enum::Mul);
-	case '[': return Add(Enum::OpenSquareBracket);
-	case ']': return Add(Enum::CloseSquareBracket);
-	case '=': return AddIfNext('=', Enum::Equiv, Enum::Assign);
-	case '!': return AddIfNext('=', Enum::NotEquiv, Enum::Not);
-	case '&': return AddIfNext('&', Enum::And, Enum::BitAnd);
-	case '|': return AddIfNext('|', Enum::Or, Enum::BitOr);
-	case '<': return AddIfNext('=', Enum::LessEquiv, Enum::Less);
-	case '>': return AddIfNext('=', Enum::GreaterEquiv, Enum::Greater);
+//	case '@': return Add(Enum::Lookup);
+//	case '*': return Add(Enum::Mul);
+//	case '[': return Add(Enum::OpenSquareBracket);
+//	case ']': return Add(Enum::CloseSquareBracket);
+//	case '=': return AddIfNext('=', Enum::Equiv, Enum::Assign);
+//	case '!': return AddIfNext('=', Enum::NotEquiv, Enum::Not);
+//	case '&': return AddIfNext('&', Enum::And, Enum::BitAnd);
+//	case '|': return AddIfNext('|', Enum::Or, Enum::BitOr);
+//	case '<': return AddIfNext('=', Enum::LessEquiv, Enum::Less);
+//	case '>': return AddIfNext('=', Enum::GreaterEquiv, Enum::Greater);
 	case '"': return LexString();
 	case '\'': return LexAlpha();
 	case '\t': return Add(Enum::Tab);
 	case '\n': return Add(Enum::NewLine);
-	case '-':
-		if (Peek() == '-')
-			return AddTwoCharOp(Enum::Decrement);
-		if (Peek() == '=')
-			return AddTwoCharOp(Enum::MinusAssign);
-		return Add(Enum::Minus);
-
-	case '.':
-		if (Peek() == '.')
-		{
-			Next();
-			if (Peek() == '.')
-			{
-				Next();
-				return Add(Enum::Replace, 3);
-			}
-			return Fail("Two dots doesn't work");
-		}
-		return Add(Enum::Dot);
-
-	case '+':
-		if (Peek() == '+')
-			return AddTwoCharOp(Enum::Increment);
-		if (Peek() == '=')
-			return AddTwoCharOp(Enum::PlusAssign);
-		return Add(Enum::Plus);
+//	case '-':
+//		if (Peek() == '-')
+//			return AddTwoCharOp(Enum::Decrement);
+//		if (Peek() == '=')
+//			return AddTwoCharOp(Enum::MinusAssign);
+//		return Add(Enum::Minus);
+//
+//	case '.':
+//		if (Peek() == '.')
+//		{
+//			Next();
+//			if (Peek() == '.')
+//			{
+//				Next();
+//				return Add(Enum::Replace, 3);
+//			}
+//			return Fail("Two dots doesn't work");
+//		}
+//		return Add(Enum::Dot);
+//
+//	case '+':
+//		if (Peek() == '+')
+//			return AddTwoCharOp(Enum::Increment);
+//		if (Peek() == '=')
+//			return AddTwoCharOp(Enum::PlusAssign);
+//		return Add(Enum::Plus);
 
 	case '/':
 		if (Peek() == '/')
@@ -90,8 +88,7 @@ bool TauLexer::NextToken()
 				;
 			return Add(Enum::Comment, offset - start);
 		}
-		return Add(Enum::Divide);
-			 */
+		Fail("Expected comment start");
 	}
 
 	LexError("Unrecognised %c");
