c96ab297fd8481976218fbd208306932825745e0
Christian <christian.schladetsch@gmail.com>
2016-05-28 20:21:56 +1000

    Added frame work for Tau IDL system

--- Library/NetworkGen/Include/Tau/Tau.h
+++ Library/NetworkGen/Include/Tau/Tau.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <KAI/Language/Common/LangCommon.h>
-#include "KAI/Language/Tau/TauTranslator.h"
+#include "Tau/TauTranslator.h"
 
 KAI_BEGIN
 
-/// Tau is an interface lagnauge used to describe an Agent in the system
+/// Tau is an interface language used to describe an Agent in the system
 /// It is wht is commonly called an 'IDL'
 class TauLang : Lang<TauTranslator>
 {
@@ -13,7 +13,6 @@ class TauLang : Lang<TauTranslator>
 
 	TauLang(Registry &r) : Parent(r) { }
 
-	Pointer<Executor> Exec(const char *text, Structure st = Structure::Expression);
 	Pointer<Continuation> Translate(const char *text, Structure st = Structure::Expression);
 	Pointer<Continuation> TranslateFile(const char *name, Structure st = Structure::Program);
 
