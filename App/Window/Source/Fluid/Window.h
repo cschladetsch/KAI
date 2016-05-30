2e860a6d2b49ecdece1c3d0273c235c1b54c9fe8
Christian <christian@twobulls.com>
2016-05-30 19:57:54 +1000

    First working FLTK Window

--- App/Window/Source/Fluid/Window.h
+++ App/Window/Source/Fluid/Window.h
@@ -6,9 +6,12 @@
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Input.H>
 extern Fl_Input *KaiInput;
-#include <FL/Fl_Button.H>
-extern Fl_Button *KaiEnter;
 #include <FL/Fl_Text_Display.H>
 extern Fl_Text_Display *KaiOutputData;
 extern Fl_Text_Display *KaiOutputContext;
+#include <FL/Fl_Return_Button.H>
+extern Fl_Return_Button *KaiEnter;
+#include <FL/Fl_Menu_Bar.H>
+extern Fl_Menu_Item menu_[];
+void StartKai();
 #endif
