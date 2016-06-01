fd60a2150ae1886a721eabf919fecf5464af996d
Christian <christian@twobulls.com>
2016-06-01 19:34:11 +1000

    Added RakNet
    Former-commit-id: e05409610f8f63cc8bf4f7e9ca8614fb2d560216
    Former-commit-id: d5b4e2b59b189c59e2184353ffeb45f5726cada5
    Former-commit-id: 6e7752a17441ef650ee5a032c70a71cf723bd317

--- Include/KAI/Network/Node.h
+++ Include/KAI/Network/Node.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <memory>
-#include <boost/asio.hpp>
-#include <boost/thread.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/bind.hpp>
-
 #include "KAI/Network/Network.h"
 
 KAI_NET_BEGIN
@@ -26,7 +20,6 @@ struct Node
 
 private:
 	std::shared_ptr<Registry> _reg;
-	std::shared_ptr<boost::asio::io_service> _io;
 
 private:
 	typedef std::unordered_map<NetHandle, std::shared_ptr<ProxyBase>, HashNetHandle> Proxies;
