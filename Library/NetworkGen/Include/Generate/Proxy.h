a0104db6b0b98910fb54fcf98e207c51929ffc3c
Christian Schladetsch <christian.schladetsch@gmail.com>
2016-06-07 01:11:42 +1000

    Pulled proxy generator out of test suite, into own namespace

---
+++ Library/NetworkGen/Include/Generate/Proxy.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <KAI/Language/Common/Process.h>
+#include <Tau/TauParser.h>
+
+// TODO: hide these under pimpl
+#include <strstream>
+#include <iostream>
+#include <string>
+
+TAU_BEGIN
+
+	namespace Generate
+	{
+		struct Proxy : Process
+		{
+			bool Generate(TauParser const &p, const char *fname);
+
+		private:
+			std::stringstream _str;
+			int _indentation = 0;
+
+			std::string EndLine();
+
+			bool Namespace(TauParser::AstNode const &ns);
+			bool Class(TauParser::AstNode const &cl);
+			bool Property(TauParser::AstNode const &prop);
+			bool Method(TauParser::AstNode const &method);
+		};
+	}
+
+TAU_END
+
+
+
--- Library/NetworkGen/Source/Tau/TauAstNode.cpp
+++ Library/NetworkGen/Source/Tau/TauAstNode.cpp
@@ -6,11 +6,16 @@ const char *TauAstEnumType::ToString(Enum ty)
 {
 	switch (ty)
 	{
-		#define CASE(N) case TauAstNodes::N : return #N;
-		#define CASE_LOWER(N) case TauAstNodes::N : return ToLower(#N);
-		#define CASE_REPLACE(N, M) case TauAstNodes::N : return M;
+		#define CASE(N) case TauAstEnumType::N : return #N;
+		#define CASE_LOWER(N) case TauAstEnumType::N : return ToLower(#N);
+		#define CASE_REPLACE(N, M) case TauAstEnumType::N : return M;
+		CASE(Namespace)
+		CASE(Class)
+		CASE(Property)
+		CASE(Method)
+		CASE(TokenType)
+		CASE(Module)
 	}
-	return "???";
 }
 
 TAU_END
