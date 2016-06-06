2b354d2bcdf46ccda2c6d093f90a0ff021335764
Christian Schladetsch <christian.schladetsch@gmail.com>
2016-06-07 01:11:42 +1000

    Pulled proxy generator out of test suite, into own namespace
    Former-commit-id: a0104db6b0b98910fb54fcf98e207c51929ffc3c
    Former-commit-id: cd39dd4e6ab962f59eee21378d2380e8231bf90b
    Former-commit-id: 45ea6986523eb854f354b78c8dbadfd59a002df0

--- Test/TestTau/Source/TestTau.cpp
+++ Test/TestTau/Source/TestTau.cpp
@@ -2,6 +2,7 @@
 #include <strstream>
 #include <iostream>
 #include <Tau/TauParser.h>
+#include <Generate/Proxy.h>
 
 using namespace std;
 using namespace kai;
@@ -36,110 +37,6 @@ TEST_F(TauTest, TestLex1)
 //			Tok::Tab, Tok::Tab, Tok::Ident, Tok::Whitespace, Tok::Ident, Tok::OpenParan,Tok::CloseParan, Tok::NewLine };
 }
 
-struct ProxyGenerator : Process
-{
-	stringstream _str;
-	int _indentation = 0;
-
-	bool Generate(TauParser const &p, const char *fname)
-	{
-		TauParser::AstNodePtr root = p.GetRoot();
-		switch (root->GetType())
-		{
-			case TauAstEnumType::Namespace:
-			{
-				if (!GenerateNameSpace(*root))
-					return false;
-				fstream f(fname);
-				const string &s = _str.str();
-				return f.write(s.c_str(), s.size()).good();
-			}
-		}
-
-		return Fail("Namespace expected");
-	}
-
-	string EndLine()
-	{
-		_str << "\n";
-		return move(string(_indentation, '\t'));
-	}
-
-	bool GenerateNameSpace(TauParser::AstNode const &ns)
-	{
-		_str << "namespace " << ns.GetToken().Text() << EndLine() << '{';
-		_indentation++;
-		for (auto const &ch : ns.GetChildren())
-		{
-			switch (ch->GetType())
-			{
-			case TauAstEnumType::Class:
-				return GenerateClass(*ch);
-			}
-		}
-		_indentation--;
-		return false;
-	}
-
-	bool GenerateClass(TauParser::AstNode const &cl)
-	{
-		_str << "class " << cl.GetToken().Text() << EndLine() << '{';
-		_indentation++;
-		for (const auto &member : cl.GetChildren())
-		{
-			switch (member->GetType())
-			{
-			case TauAstEnumType::Property:
-				if (!GenerateProperty(*member))
-					return false;
-
-			case TauAstEnumType::Method:
-				if (!GenerateMethod(*member))
-					return false;
-
-			default:
-				return Fail("Unknown class member: %s"
-					, TauAstEnumType::ToString(member->GetType()));
-			}
-		}
-
-		_indentation--;
-		_str << '}' << EndLine();
-	}
-
-	bool GenerateProperty(TauParser::AstNode const &prop)
-	{
-		_str
-			<< "IFuture<" << prop.GetChild(0)->GetTokenText() << "> "
-			<< prop.GetChild(1)->GetTokenText() << ';' << EndLine();
-		return true;
-	}
-
-	bool GenerateMethod(TauParser::AstNode const &method)
-	{
-		auto const &rt = method.GetChild(0);
-		auto const &name = method.GetChild(1);
-		auto const &args = method.GetChild(2);
-//		auto const &konst = method.GetChild(3);
-
-		_str << "IFuture<" << rt->GetTokenText() << "> " << name->GetTokenText() << "(";
-		bool first = true;
-		for (auto const &a : args->GetChildren())
-		{
-			if (!first)
-				_str << ", ";
-
-			auto &ty = a->GetChild(0);
-			auto &id = a->GetChild(1);
-			_str << ty->GetTokenText() << " " << id->GetTokenText();
-
-			first = false;
-		}
-
-		_str << ';' << EndLine();
-	}
-};
-
 TEST_F(TauTest, TestParse)
 {
 	try
@@ -156,10 +53,10 @@ TEST_F(TauTest, TestParse)
 		p->Process(l, Structure::Namespace);
 		if (p->Failed)
 			cerr << p->Error << endl;
-		cout << p->PrintTree();
 		ASSERT_FALSE(p->Failed);
+		cout << p->PrintTree();
 
-		ProxyGenerator g;
+		Generate::Proxy g;
 		EXPECT_TRUE(g.Generate(*p, "TestParse.int"));
 	}
 	catch (Exception::Base &e)
