23d08da9fe61ead754279b8528b2855a12c6ec40
Christian Schladetsch <christian.schladetsch@gmail.com>
2016-06-06 19:35:43 +1000

    Added Proxy generator in test case
    Former-commit-id: 6bd858a9cc530ac0dc8938855946de201939bb1e
    Former-commit-id: 630aa6c33f15762530abcc8ac20e3c889b4ba3a9
    Former-commit-id: 57cd7cb62b299da734706c51ca14a6eacb8a0286

--- Library/NetworkGen/Source/Tau/TauParser.cpp
+++ Library/NetworkGen/Source/Tau/TauParser.cpp
@@ -1,6 +1,8 @@
 #include <KAI/Language/Common/ParserCommon.h>
 #include <Tau/TauParser.h>
 
+using namespace std;
+
 TAU_BEGIN
 
 void TauParser::Process(std::shared_ptr<Lexer> lex, Structure st)
@@ -18,634 +20,141 @@ void TauParser::Process(std::shared_ptr<Lexer> lex, Structure st)
 
 	// strip whitespace and comments
 	for (auto tok : lexer->GetTokens())
-		if (tok.type != TokenEnum::Whitespace && tok.type != TokenEnum::Comment)
-			tokens.push_back(tok);
+	{
+		switch (tok.type)
+		{
+		case TokenEnum::Whitespace:
+		case TokenEnum::Comment:
+		case TokenEnum::NewLine:
+			continue;
+		}
 
-	root = NewNode(AstEnum::Namespace);
+		tokens.push_back(tok);
+	}
+
+	root = NewNode(AstEnum::Module);
 
 	KAI_UNUSED_1(st);
 	Run(Structure::Namespace);
 }
 
-void TauParser::Walk(std::function<bool(AstNodePtr)> fun)
-{
-
-}
-
 void TauParser::Run(Structure st)
 {
-	switch (st)
+	KAI_UNUSED_1(st);
+	if (tokens.empty())
+		return;
+
+	while (Current().type == TokenType::Namespace)
 	{
-		case Structure::Namespace:
-		{
-			auto ns = Expect(TokenEnum::Namespace);
-			Namespace(ns);
-			root->Add(ns);
-			break;
-		}
+		Consume();
+		Namespace(root);
+		if (Failed)
+			return;
 	}
+
+	// TODO: check for invalid trailing text outside of a namespace
 }
 
 void TauParser::Namespace(AstNodePtr root)
 {
-	Expect(TokenEnum::Ident);
-	root->Add(Consume());
-	Expect(TokenEnum::NewLine);
-	Expect(TokenEnum::Tab);
+	auto ns = make_shared<AstNode>(TauAstEnumType::Namespace, Consume());
+	Expect(TokenEnum::OpenBrace);
 
 	while (Peek().type == TokenEnum::Class)
 	{
-		Class(root);
+		Consume();
+		Class(ns);
+		if (Failed)
+			return;
 	}
+
+	Expect(TokenEnum::CloseBrace);
+	root->Add(ns);
 }
 
 void TauParser::Class(AstNodePtr root)
 {
-	root->Add(Consume());
-}
+	auto cl = make_shared<AstNode>(TauAstEnumType::Class);
+	cl->Add(Consume());
+	Expect(TokenEnum::OpenBrace);
 
-void TauParser::Method(AstNodePtr rent)
-{
-
-}
-
-#if 0
-
-void TauParser::Run(Structure st)
-{
-	switch (st)
+	while (Peek().type != TokenEnum::CloseBrace)
 	{
-	case Structure::Statement:
-		if (!Statement(root))
+		// Expect a series of methods and properties.
+		// Either way, start with a type name and identifier.
+		Expect(TokenEnum::Ident);
+		auto ty = Consume();
+		Expect(TokenEnum::Ident);
+		auto id = Consume();
+
+		if (Peek().type == TokenType::OpenParan)
 		{
-			CreateError("Statement expected");
-			return;
+			Consume();
+			Method(cl, ty, id);
 		}
-		break;
-
-	case Structure::Expression:
-		if (!Expression())
+		else
 		{
-			CreateError("Expression expected");
-			return;
+			Field(cl, ty, id);
 		}
-		root->Add(Pop());
-		break;
 
-	case Structure::Function:
-		Function(root);
-		break;
-
-	case Structure::Program:
-		Program();
-		break;
+		if (Failed)
+			return;
 	}
 
-	if (!stack.empty())
-		Fail("Internal error: Stack not empty after parsing");
+	Expect(TokenEnum::CloseBrace);
+
+	root->Add(cl);
 }
 
-bool TauParser::Program()
+void TauParser::Method(AstNodePtr cl, TokenNode const &ty, TokenNode const &id)
 {
-	while (!Try(TokenType::None) && !Failed)
+	auto method = make_shared<AstNode>(TauAstEnumType::Method, id);
+	method->Add(ty);
+	method->Add(id);
+
+	auto args = make_shared<AstNode>();
+	method->Add(args);
+
+	do
 	{
-		ConsumeNewLines();
-		Statement(root);
+		AddArg(args);
 	}
+	while (Peek().type == TokenType::Comma);
 
-	return true;
+	Expect(TokenType::CloseParan);
+	Expect(TokenType::Semi);
+
+	method->Add(make_shared<AstNode>());
+
+	cl->Add(method);
 }
 
-void TauParser::Function(AstNodePtr node)
+void TauParser::AddArg(AstNodePtr parent)
 {
-	ConsumeNewLines();
+	if (Peek().type != TokenType::Ident)
+		return;
 
-	Expect(TokenType::Fun);
+	auto ty = Consume();
 	Expect(TokenType::Ident);
-	TauToken name = Last();
-	std::shared_ptr<AstNode> fun = NewNode(AstEnum::Function);
-	fun->Add(name);
-	Expect(TokenType::OpenParan);
-	std::shared_ptr<AstNode> args = NewNode(AstEnum::None);
-	fun->Add(args);
+	auto name = Consume();
 
-	if (Try(TokenType::Ident))
-	{
-		args->Add(Consume());
-		while (Try(TokenType::Comma))
-		{
-			Consume();
-			args->Add(Expect(TokenType::Ident));
-		}
-	}
+	auto arg = make_shared<AstNode>();
+	arg->Add(ty);
+	arg->Add(name);
 
-	Expect(TokenType::CloseParan);
-	Expect(TokenType::NewLine);
-	
-	AddBlock(fun);
-	node->Add(fun);
+	parent->Add(arg);
 }
 
-void TauParser::Block(AstNodePtr node)
+void TauParser::Field(AstNodePtr cl, TokenNode const &ty, TokenNode const &id)
 {
-	ConsumeNewLines();
+	auto field = make_shared<AstNode>(TauAstEnumType::Property);
+	field->Add(ty);
+	field->Add(id);
 
-	++indent;
-	while (!Failed)
-	{
-		int level = 0;
-		while (Try(TokenType::Tab))
-		{
-			++level;
-			Consume();
-		}
+	Expect(TokenType::Semi);
 
-		if (Try(TokenType::NewLine))
-		{
-			Consume();
-			continue;
-		}
-
-		// close current block
-		if (level < indent)
-		{
-			--indent;
-
-			// rewind to start of tab sequence to determine next block
-			--current;
-			while (Try(TokenType::Tab))
-				--current;
-
-			++current;
-			return;
-		}
-
-		if (level != indent)
-		{
-			Fail(Lexer::CreateErrorMessage(Current(), "Mismatch block indent"));
-			return;
-		}
-
-		Statement(node);
-	}
+	cl->Add(field);
 }
-
-bool TauParser::Statement(AstNodePtr block)
-{
-	switch (Current().type)
-	{
-		case TokenType::Assert:
-		{
-			Consume();
-			if (!Expression())
-			{
-				Fail(Lexer::CreateErrorMessage(Current(), "Assert needs an expression to test"));
-				return false;
-			}
-
-			auto ass = NewNode(Consume());
-			ass->Add(Pop());
-			Push(ass);
-			goto finis;
-		}
-
-		case TokenType::Return:
-		case TokenType::Yield:
-		{
-			auto ret = NewNode(Consume());
-			if (Expression())
-				ret->Add(Pop());
-			block->Add(ret);
-			goto finis;
-		}
-		
-		case TokenType::While:
-		{
-			While(block);
-			return true;
-		}
-
-		case TokenType::For:
-		{
-			For(block);
-			return true;
-		}
-
-		case TokenType::If:
-		{
-			IfCondition(block);
-			return true;
-		}
-		
-		case TokenType::Fun:
-		{
-			Function(block);
-			return true;
-		}
-	}
-
-	if (!Expression())
-		return false;
-
-	block->Add(Pop());
-
-finis:
-	// statements can end with an optional semi followed by a new line
-	if (Try(TokenType::Semi))
-		Consume();
-
-	Expect(TokenType::NewLine);
-
-	return true;
-}
-
-bool TauParser::Expression()
-{
-	bool paran = Try(TokenType::OpenParan);
-	if (paran)
-	{
-		Consume();
-		if (!Expression())
-			return false;
-
-		Expect(TokenType::CloseParan);
-		return true;
-	}
-
-	if (!Logical())
-		return false;
-
-	if (Try(TokenType::Assign) || Try(TokenType::PlusAssign) || Try(TokenType::MinusAssign) || Try(TokenType::MulAssign) || Try(TokenType::DivAssign))
-	{
-		auto node = NewNode(Consume());
-		auto ident = Pop();
-		if (!Logical())
-		{
-			Fail(Lexer::CreateErrorMessage(Current(), "Assignment requires an expression"));
-			return false;
-		}
-
-		node->Add(Pop());
-		node->Add(ident);
-		Push(node);
-	}
-
-	return true;
-}
-
-bool TauParser::Logical()
-{
-	if (!Relational())
-		return false;
-
-	while (Try(TokenType::And) || Try(TokenType::Or))
-	{
-		auto node = NewNode(Consume());
-		node->Add(Pop());
-		if (!Relational())
-			return Fail(Lexer::CreateErrorMessage(Current(), "Relational expected"));
-
-		node->Add(Pop());
-		Push(node);
-	}
-
-	return true;
-}
-
-bool TauParser::Relational()
-{
-	if (!Additive())
-		return false;
-
-	while (Try(TokenType::Less) || Try(TokenType::Greater) || Try(TokenType::Equiv) || Try(TokenType::NotEquiv)
-		|| Try(TokenType::LessEquiv) || Try(TokenType::GreaterEquiv))
-	{
-		auto node = NewNode(Consume());
-		node->Add(Pop());
-		if (!Additive())
-			return CreateError("Additive expected");
-
-		node->Add(Pop());
-		Push(node);
-	}
-
-	return true;
-}
-
-bool TauParser::Additive()
-{
-	// unary +/- operator
-	if (Try(TokenType::Plus) || Try(TokenType::Minus))
-	{
-		auto ty = Consume().type;
-		Consume();
-		if (!Term())
-			return CreateError("Term expected");
-
-		auto node = new AstNode(ty == TokenType::Plus ? NodeType::Positive : NodeType::Negative);
-		node->Add(Pop());
-		return true;
-	}
-
-	if (!Term())
-		return false;
-
-	while (Try(TokenType::Plus) || Try(TokenType::Minus))
-	{
-		auto node = NewNode(Consume());
-		node->Add(Pop());
-		if (!Term())
-			return CreateError("Term expected");
-
-		node->Add(Pop());
-		Push(node);
-	}
-
-	return true;
-}
-
-bool TauParser::Term()
-{
-	if (!Factor())
-		return false;
-
-	while (Try(TokenType::Mul) || Try(TokenType::Divide))
-	{
-		auto node = NewNode(Consume());
-		node->Add(Pop());
-		if (!Factor())
-			return CreateError("Factor expected with a term");
-
-		node->Add(Pop());
-		Push(node);
-	}
-
-	return true;
-}
-
-bool TauParser::Factor()
-{
-	if (Try(TokenType::OpenParan))
-	{
-		Consume();
-		if (!Expression())
-		{
-			CreateError("Expected an expression for a factor in parenthesis");
-
-			return false;
-		}
-
-		Expect(TokenType::CloseParan);
-		return true;
-	}
-
-	if (Try(TokenType::OpenSquareBracket))
-	{
-		auto list = NewNode(NodeType::List);
-		do
-		{
-			Consume();
-			if (Expression())
-				list->Add(Pop());
-			else
-				list->Add(0);
-		}
-		while (Try(TokenType::Comma));
-
-		Expect(TokenType::CloseSquareBracket);
-		Push(list);
-
-		return true;
-	}
-
-	if (Try(TokenType::Int) || Try(TokenType::Float) || Try(TokenType::String) || Try(TokenType::True) || Try(TokenType::False))
-		return PushConsume();
-
-	if (Try(TokenType::Self))
-		return PushConsume();
-
-	while (Try(TokenType::Lookup))
-		PushConsume();
-
-	if (Try(TokenType::Ident))
-		return ParseFactorIdent();
-
-	return false;
-}
-
-//warning C4127: conditional expression is constant
-#pragma warning (disable:4127)
-
-bool TauParser::ParseFactorIdent()
-{
-	PushConsume();
-
-	while (true)
-	{
-		if (Try(TokenType::Dot))
-		{
-			ParseGetMember();
-			continue;
-		}
-
-		if (Try(TokenType::OpenParan))
-		{
-			ParseMethodCall();
-			continue;
-		}
-
-		if (Try(TokenType::OpenSquareBracket))
-		{
-			ParseIndexOp();
-			continue;
-		}
-
-		break;
-	}
-
-	return true;
-}
-
-void TauParser::ParseMethodCall()
-{
-	Consume();
-	auto call = NewNode(NodeType::Call);
-	call->Add(Pop());
-	auto args = NewNode(NodeType::ArgList);
-	call->Add(args);
-
-	if (Expression())
-	{
-		args->Add(Pop());
-		while (Try(TokenType::Comma))
-		{
-			Consume();
-			if (!Expression())
-			{
-				Fail("What is the next argument?");
-				return;
-			}
-
-			args->Add(Pop());
-		}
-	}
-
-	Push(call);
-	Expect(TokenType::CloseParan);
-
-	if (Try(TokenType::Replace))
-		call->Add(Consume());
-}
-
-void TauParser::ParseGetMember()
-{
-	Consume();
-	auto get = NewNode(NodeType::GetMember);
-	get->Add(Pop());
-	get->Add(Expect(TokenType::Ident));
-	Push(get);
-}
-
-void TauParser::IfCondition(AstNodePtr block)
-{
-	if (!Try(TokenType::If))
-		return;
-
-	Consume();
-
-	// get the test expression
-	if (!Expression())
-	{
-		CreateError("If what?");
-		return;
-	}
-
-	auto condition = Pop();
-	//Expect(TokenType::CloseParan);
-
-	// get the true-clause
-	auto trueClause = NewNode(NodeType::Block);
-	Block(trueClause);
-
-	// make the conditional node in AST
-	auto cond = NewNode(NodeType::Conditional);
-	cond->Add(condition);
-	cond->Add(trueClause);
-
-	// if there's an else, add it as well
-	if (Try(TokenType::Else))
-	{
-		Consume();
-		auto falseClause = NewNode(NodeType::Block);
-		Block(falseClause);
-		cond->Add(falseClause);
-	}
-
-	block->Add(cond);
-}
-
-void TauParser::ParseIndexOp()
-{
-	Consume();
-	auto index = NewNode(NodeType::IndexOp);
-	index->Add(Pop());
-	if (!Expression())
-	{
-		CreateError("Index what?");
-		return;
-	}
-
-	Expect(TokenType::CloseSquareBracket);
-	index->Add(Pop());
-	Push(index);
-}
-
-void TauParser::For(AstNodePtr block)
-{
-	if (!Try(TokenType::For))
-		return;
-
-	Consume();
-
-	auto f = NewNode(TauAstNodeEnumType::For);
-	if (!Expression())
-	{
-		CreateError("For what?");
-		return;
-	}
-
-	if (Try(TokenType::In))
-	{
-		Consume();
-		f->Add(Pop());
-
-		if (!Expression())
-		{
-			CreateError("For each in what?");
-			return;
-		}
-
-		f->Add(Pop());
-	}
-	else
-	{
-		Expect(TokenType::Semi);
-		f->Add(Pop());
-
-		if (!Expression())
-		{
-			CreateError("When does the for statement stop?");
-			return;
-		}
-
-		f->Add(Pop());
-		Expect(TokenType::Semi);
-
-		if (!Expression())
-		{
-			CreateError("What happens when a for statement ends?");
-			return;
-		}
-
-		f->Add(Pop());
-	}
-
-	Expect(TokenType::NewLine);
-	AddBlock(f);
-	block->Add(f);
-}
-
-void TauParser::While(AstNodePtr block)
-{
-	auto w = NewNode(Consume());
-	if (!Expression())
-	{
-		CreateError("While what?");
-		return;
-	}
-
-	w->Add(Pop());
-	block->Add(w);
-}
-
-bool TauParser::CreateError(const char *text)
-{
-	return Fail(Lexer::CreateErrorMessage(Current(), text));
-}
-
-void TauParser::AddBlock(AstNodePtr fun)
-{
-	auto block = NewNode(TauAstNodeEnumType::Block);
-	Block(block);
-	fun->Add(block);
-}
-
-void TauParser::ConsumeNewLines()
-{
-	while (Try(TokenType::NewLine))
-		Consume();
-}
-
-#endif
 
 TAU_END
+
--- Test/TestGamma/Readme.md
+++ Test/TestGamma/Readme.md
@@ -1,2 +1,4 @@
 # Gamma
-This is a lame attempt at writing a vi-like editor to eventually be used for KAI in the  xWidgets systems.
+
+This is a lame attempt at writing a vi-like editor to eventually 
+be used for KAI in the fltk systems.
