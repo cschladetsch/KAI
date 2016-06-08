4ee20f96a44ce3041f1a33432793dcd8d1bcf824
Christian Schladetsch <christian.schladetsch@gmail.com>
2016-06-09 01:35:52 +1000

    Cleaning

--- Test/TestTau/Source/TestTau.cpp
+++ Test/TestTau/Source/TestTau.cpp
@@ -23,20 +23,6 @@ struct TauTest : ::testing::Test
 	}
 };
 
-TEST_F(TauTest, TestLex1)
-{
-	Registry r;
-	TauLexer l(input, r);
-	l.Process();
-	if (l.Failed)
-		cerr << l.Error << endl;
-//
-//	TauTokenEnumType::Enum res[] = {
-//			Tok::Namespace, Tok::Whitespace, Tok::Ident, Tok::NewLine,
-//			Tok::Tab, Tok::Class, Tok::Whitespace, Tok::Ident, Tok::NewLine,
-//			Tok::Tab, Tok::Tab, Tok::Ident, Tok::Whitespace, Tok::Ident, Tok::OpenParan,Tok::CloseParan, Tok::NewLine };
-}
-
 TEST_F(TauTest, TestParse)
 {
 	try
@@ -47,14 +33,14 @@ TEST_F(TauTest, TestParse)
 		if (l->Failed)
 			cerr << l->Error << endl;
 		ASSERT_FALSE(l->Failed);
-		cout << l->Print() << endl;
+//		cout << l->Print() << endl;
 
 		auto p = make_shared<TauParser>(r);
 		p->Process(l, Structure::Namespace);
 		if (p->Failed)
 			cerr << p->Error << endl;
 		ASSERT_FALSE(p->Failed);
-		cout << p->PrintTree();
+//		cout << p->PrintTree();
 
 		Generate::Proxy g;
 		EXPECT_TRUE(g.Generate(*p, "TestParse.proxy.h"));
@@ -82,10 +68,5 @@ TEST_F(TauTest, TestGenerate)
 	auto p = make_shared<TauParser>(r);
 	p->Process(l, Structure::Namespace);
 	ASSERT_FALSE(p->Failed);
-
-//	auto t = new TauTranslator(r);
-//	t->GenerateInterface("TestInterface.h", *p);
-//	t->GenerateInterface("TestProxy.h", *p);
-//	t->GenerateInterface("TestAgent.h", *p);
 }
 
