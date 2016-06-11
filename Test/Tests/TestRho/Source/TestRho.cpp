71d7372c3075c2bdba31b14b3f70cfbc5cc3592b
Christian Schladetsch <christian.schladetsch@gmail.com>
2016-06-11 21:10:12 +1000

    Better error reporting.
    Former-commit-id: 2ef2024afa6b06a8edd2366ebf54564b322b4329
    Former-commit-id: d5ab89dba8ffbf2e823c9a8202de3ee8ca7cb95f
    Former-commit-id: c1b1829cb7bc1d400ee16186b0dc0333fc4ee6d0

--- Test/Tests/TestRho/Source/TestRho.cpp
+++ Test/Tests/TestRho/Source/TestRho.cpp
@@ -3,18 +3,32 @@
 using namespace kai;
 using namespace std;
 
-
 TEST(TestRho, TestAssignment)
 {
-	TranslatorCommon::trace = 2;
+	try
+	{
+		TranslatorCommon::trace = 2;
 
-	Console console;
-	console.SetLanguage(Language::Rho);
-	console.Execute("a=1;");
+		Console console;
+		console.SetLanguage(Language::Rho);
+		console.Execute("a=1;");
 
-	Object root = console.GetRoot();
-	Label name("a");
-	ASSERT_TRUE(root.HasChild(name));
-	ASSERT_EQ(Deref<int>(root.GetChild(name)), 3);
+		Object root = console.GetRoot();
+		Label name("a");
+		ASSERT_TRUE(root.HasChild(name));
+		ASSERT_EQ(Deref<int>(root.GetChild(name)), 3);
+	}
+	catch (Exception::Base &e)
+	{
+		cerr << e.ToString() << endl;
+	}
+	catch (std::exception &e)
+	{
+		cerr << e.what() << endl;
+	}
+	catch (...)
+	{
+		cerr << "Exception" << endl;
+	}
 }
 
