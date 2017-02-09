be1d09c1e389f137e453c9606d62a36b4f2268dd
Christian <christian@twobulls.com>
2017-02-10 03:07:34 +1100

    Fixed operator<< for StringStream and Structure
    Former-commit-id: 1dae83e08110767d8d419050a3af20f0df5e8cf1
    Former-commit-id: 2a79426dfcfb2e9f14901d18bee53c6220c6b24b
    Former-commit-id: 82b27e41b6196efe78d9302ca32af0c73fffb980

--- Include/KAI/Core/Debug.h
+++ Include/KAI/Core/Debug.h
@@ -12,8 +12,8 @@ KAI_BEGIN
 
 extern void DebugTrace(const char *);
 
-	StringStream &operator<<(StringStream &S, const Structure &T);
-	StringStream &operator>>(StringStream &S, Structure &T);
+StringStream &operator<<(StringStream &S, const Structure &T);
+StringStream &operator>>(StringStream &S, Structure &T);
 
 namespace debug
 {
