f8f53865dbebf7a0944913c145bae8131263227e
Christian <christian@twobulls.com>
2017-02-12 19:27:43 +1100

    Added skeleton for TestRegistry test suite.
    Former-commit-id: 705b7c9440a3d22a634f87f7f2ab5becf6079e1d
    Former-commit-id: c2f747cfcf6e28cbd05ebcd5a2f9049e0970e43a
    Former-commit-id: 79ad11101df328db8cc63e1539f4908f7c479e3b

--- Test/Tests/TestRegistry/Source/Main.cpp
+++ Test/Tests/TestRegistry/Source/Main.cpp
@@ -1,4 +1,4 @@
-#include "Main.h"
+#include "./Main.h"
 
 int main(int argc, char **argv)
 {
--- Test/Tests/TestRegistry/Source/TestRegistry.cpp
+++ Test/Tests/TestRegistry/Source/TestRegistry.cpp
@@ -1,4 +1,26 @@
-#include "Main.h"
+#include "./Main.h"
+#include <KAI/Core/BuiltinTypes.h>
+
+USING_NAMESPACE_KAI
+
+class TestRegistry : public ::testing::Test
+{
+	Registry _reg;
+
+protected:
+	virtual void SetUp() override
+	{
+		_reg.AddClass<void>();
+		_reg.AddClass<int>();
+		_reg.AddClass<bool>();
+		_reg.AddClass<String>();
+		// _reg.AddClass<StringStream>();
+	}	
+
+	virtual void TearDown() override
+	{
+	}
+};
 
 TEST_F(TestRegistry, TestInit)
 {
