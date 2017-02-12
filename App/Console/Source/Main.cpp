1621bff1d97d47fe91419641ff983a6ec3ee5626
Christian <christian@twobulls.com>
2017-02-12 18:41:14 +1100

    Remove explicit WIN32 references from Console app.

--- App/Console/Source/Main.cpp
+++ App/Console/Source/Main.cpp
@@ -1,31 +1,23 @@
-#include "KAI/Console/Console.h"
-
+#include <KAI/Console/Console.h>
 #include <iostream>
-
-#ifdef WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#endif
 
 using namespace std;
 
-
 USING_NAMESPACE_KAI
-
-//static Color _color;
 
 int main(int argc, char **argv)
 {
 	KAI_UNUSED_2(argc, argv);
 
-	cout << "pyro v0.1" << endl;
+	cout << "KAI v0.1" << endl;
 
 	Console console;
 	console.SetLanguage(Language::Pi);
 
+	// the higher the number, the greater the verbosity of debug output
 	Process::trace = 0;
 	console.GetExecutor()->SetTraceLevel(0);
 
+	// start the REPL
 	return console.Run();
 }
-
