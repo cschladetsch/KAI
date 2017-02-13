ff11af4ce7242abb56c5127d1fbb6517e25d88ac
Christian <christian.schladetsch@gmail.com>
2017-02-13 18:24:07 +1100

    Greatly simplified the filesystem struture of all test systems.

---
+++ Test/Source/TestSet.cpp
@@ -0,0 +1,43 @@
+#include "./Common.h"
+
+
+USING_NAMESPACE_KAI
+
+class TestSet : public ::testing::Test
+{
+	virtual void SetUp() override
+	{
+	}	
+
+	virtual void TearDown() override
+	{
+	}
+};
+
+TEST_F(TestSet, TestCreation)
+{
+}
+
+TEST_F(TestSet, TestInsert)
+{
+}
+
+TEST_F(TestSet, TestDelete)
+{
+}
+
+TEST_F(TestSet, TestOwnership)
+{
+}
+
+TEST_F(TestSet, TestComparison)
+{
+}
+
+TEST_F(TestSet, TestStringStream)
+{
+}
+
+TEST_F(TestSet, TestBinaryStream)
+{
+}
