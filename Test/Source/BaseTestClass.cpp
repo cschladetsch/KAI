851c2ebce8bbf15288e33d82f8b6bf5013ac97b5
Christian <christian.schladetsch@gmail.com>
2017-02-13 18:24:07 +1100

    Greatly simplified the filesystem struture of all test systems.
    Former-commit-id: ff11af4ce7242abb56c5127d1fbb6517e25d88ac
    Former-commit-id: c6bfec74eb4f7812cbab3da7af9a30638e888ea8
    Former-commit-id: 6bb195769e3cea67a9d5c7b0ff46aa936c11b3c0

---
+++ Test/Source/BaseTestClass.cpp
@@ -0,0 +1,29 @@
+#include "./Common.h"
+
+KAI_BEGIN
+
+void KAITestClass::SetUp()
+{
+	reg.AddClass<void>();
+	reg.AddClass<bool>();
+	reg.AddClass<int>();
+	reg.AddClass<String>();
+
+	auto root = reg.New<void>();
+	tree.SetRoot(root);
+	reg.SetTree(tree);
+
+	AddrequiredClasses();
+}	
+
+void KAITestClass::AddrequiredClasses() 
+{
+}
+
+void KAITestClass::TearDown()
+{
+	reg.Clear();
+}
+
+KAI_END
+
---
+++ Test/Source/TestList.cpp
@@ -0,0 +1,43 @@
+#include "./Common.h"
+
+
+USING_NAMESPACE_KAI
+
+class TestList : public ::testing::Test
+{
+	virtual void SetUp() override
+	{
+	}	
+
+	virtual void TearDown() override
+	{
+	}
+};
+
+TEST_F(TestList, TestCreation)
+{
+}
+
+TEST_F(TestList, TestInsert)
+{
+}
+
+TEST_F(TestList, TestDelete)
+{
+}
+
+TEST_F(TestList, TestOwnership)
+{
+}
+
+TEST_F(TestList, TestComparison)
+{
+}
+
+TEST_F(TestList, TestStringStream)
+{
+}
+
+TEST_F(TestList, TestBinaryStream)
+{
+}
