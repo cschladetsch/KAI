7147ea74c146ddeaad34575d52469308b33cbdd4
Christian <christian.schladetsch@gmail.com>
2017-02-19 06:52:26 +1100

    Consolidated Readme's
    Former-commit-id: 42b8704070e73c1991a8abe26f332690197822d6
    Former-commit-id: 2e6655b33ad382935343fed17d077c58e4278dec
    Former-commit-id: 7d079f0f7266af056bed8c6543ed9dd2bf46081a

--- build/_cmt_tmp.cmake
+++
@@ -1,4 +0,0 @@
-
-            set(_args "Peer;/Users/christian/Local/KAI/App/NetworkPeer/Main.cpp;/Users/christian/Local/KAI/App/NetworkPeer/Peer.cpp")
-            _add_executable(${_args})
-        
--- lib/Readme.md
+++
@@ -1,4 +0,0 @@
-# Libraries
-
-Here is where all the static and dynamic KAI libraries are written to.
-
