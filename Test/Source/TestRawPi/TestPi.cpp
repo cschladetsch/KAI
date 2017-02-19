958e494eb7add977e324aff714eac74d8f436bd8
Christian <christian.schladetsch@gmail.com>
2017-02-19 14:00:20 +1100

    Added suppor for Div op, added better diags for missing ops.
    Former-commit-id: 0bb6d0088e84b7745b6235e046e46739385c7e34
    Former-commit-id: 3ea59e9c5bde3d1bcaabbec40e3f2cdd9e98f9d7
    Former-commit-id: 15ea458b6f7bd152723aadaca6ae5d2633d7855f

--- Test/Source/TestRawPi/TestPi.cpp
+++ Test/Source/TestRawPi/TestPi.cpp
@@ -13,6 +13,9 @@ TEST_F(TestPi, TestArithmetic)
 
 	_console->SetLanguage(Language::Pi);
 	_data->Clear();
+	_console->Execute("6 2 div");
+	ASSERT_EQ(AtData<int>(0), 3);
+
 	_console->Execute("1 2 +");
 	auto result = AtData<int>(0);
 	ASSERT_EQ(result, 3);
@@ -25,26 +28,9 @@ TEST_F(TestPi, TestArithmetic)
 	_console->Execute("2 2 *");
 	ASSERT_EQ(AtData<int>(0), 4);
 
-	// TODONOW
-
-	// _data->Clear();
-	// _console->Execute("6 2 div");
-	// ASSERT_EQ(AtData<int>(0), 3);
-
-	// _data->Clear();
-	// _console->Execute("3 2 + 2 2 * * 2 div");
-	// ASSERT_EQ(AtData<int>(0), 10);
-}
-
-TEST_F(TestPi, TestAssignment)
-{
-	_console->SetLanguage(Language::Pi);
-	_console->Execute("3 'a");
-
-	Label name("a");
-	Object scope = GetContext().GetScope();
-	ASSERT_TRUE(scope.HasChild(name));
-	ASSERT_EQ(Deref<int>(scope.GetChild(name)), 3);
+	_data->Clear();
+	_console->Execute("3 2 + 2 2 * * 2 div");
+	ASSERT_EQ(AtData<int>(0), 10);
 }
 
 
