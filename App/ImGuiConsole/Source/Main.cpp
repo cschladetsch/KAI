b5b264bd61e93027a9a2eec5157eae310623f10d
Christian <christian.schladetsch@gmail.com>
2017-02-20 19:33:00 +1100

    Got ImGui test app building from Visual Studio Code.

--- App/ImGuiConsole/Source/Main.cpp
+++ App/ImGuiConsole/Source/Main.cpp
@@ -1,25 +1,103 @@
-#include <KAI/Console/Console.h>
 #include <iostream>
+#include <stdio.h>
+
+#include <KAI/Console/Console.h>
+#include <KAI/ImGui/imgui.h>
+#include "./imgui_impl_glfw_gl3.h"
+#include <stdio.h>
+#include <GL/gl3w.h> 
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
 USING_NAMESPACE_KAI
 
+using namespace ImGui;
+
+static void error_callback(int error, const char* description)
+{
+    cerr  << "Error " << error << ": " << description << endl;
+}
+
+GLFWwindow *SetupGui()
+{
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        return 0;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    gl3wInit();
+
+    // Setup ImGui binding
+    ImGui_ImplGlfwGL3_Init(window, true);
+
+	return window;
+}
+
 int main(int argc, char **argv)
 {
-	return -1;
+	GLFWwindow *window = SetupGui();
+	if (!window)
+		return -1;
 
-	// KAI_UNUSED_2(argc, argv);
+    bool show_test_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImColor(114, 144, 154);
 
-	// cout << "KAI v0.1" << endl;
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        ImGui_ImplGlfwGL3_NewFrame();
 
-	// Console console;
-	// console.SetLanguage(Language::Pi);
+        // 1. Show a simple window
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+        {
+            static float f = 0.0f;
+            ImGui::Text("Hello, world!");
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            if (ImGui::Button("Test Window")) show_test_window ^= 1;
+            if (ImGui::Button("Another Window")) show_another_window ^= 1;
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
 
-	// // the higher the number, the greater the verbosity of debug output
-	// Process::trace = 0;
-	// console.GetExecutor()->SetTraceLevel(0);
+        // 2. Show another simple window, this time using an explicit Begin/End pair
+        if (show_another_window)
+        {
+            ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
+            ImGui::Begin("Another Window", &show_another_window);
+            ImGui::Text("Hello");
+            ImGui::End();
+        }
 
-	// // start the REPL
-	// return console.Run();
+        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+        if (show_test_window)
+        {
+            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+            ImGui::ShowTestWindow(&show_test_window);
+        }
+
+        // Rendering
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplGlfwGL3_Shutdown();
+    glfwTerminate();
+
+    return 0;
 }
