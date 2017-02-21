01c9c577028fe2da3bea8c41d8c794aa64fe992c
Christian <christian.schladetsch@gmail.com>
2017-02-21 19:28:23 +1100

    Fixed weird corner-cases for comments and files that are only comments.

--- Test/Source/TestRawPi/TestPi.cpp
+++ Test/Source/TestRawPi/TestPi.cpp
@@ -1,4 +1,6 @@
 #include "TestLangCommon.h"
+#include <KAI/Core/BuiltinTypes.h>
+#include <KAI/Core/BuiltinTypes/Array.h>
 
 using namespace kai;
 using namespace std;
@@ -6,6 +8,22 @@ using namespace std;
 struct TestPi : TestLangCommon
 {
 };
+
+TEST_F(TestPi, TestComments)
+{
+	_console->SetLanguage(Language::Pi);
+	_console->Execute("// text\n\n\n\n\n");
+	ASSERT_EQ(_data->Size(), 0);
+
+	_console->Execute("// text\n");
+	ASSERT_EQ(_data->Size(), 0);
+	
+	_console->Execute("// text");
+	ASSERT_EQ(_data->Size(), 0);
+
+	_console->Execute("//");
+	ASSERT_EQ(_data->Size(), 0);
+}
 
 TEST_F(TestPi, TestArithmetic)
 {
@@ -33,4 +51,23 @@ TEST_F(TestPi, TestArithmetic)
 	ASSERT_EQ(AtData<int>(0), 10);
 }
 
+TEST_F(TestPi, TestVectors)
+{
+	_console->Execute("[] type");
+	Object ty = _data->At(0);
+	ASSERT_EQ(ty.GetTypeNumber(), Type::Traits<Array>::Number);
+
+	_data->Clear();
+	_console->Execute("[1 2 3] size &");
+	ASSERT_EQ(AtData<int>(0), 3);
+
+	_data->Clear();
+	_console->Execute("1 2 3 3 >array");
+	Pointer<Array> a = _data->Top();
+	ASSERT_EQ(a->Size(), 3);
+	ASSERT_EQ(ConstDeref<int>(a->At(0)), 1);
+	ASSERT_EQ(ConstDeref<int>(a->At(1)), 2);
+	ASSERT_EQ(ConstDeref<int>(a->At(2)), 3);
+
+}
 
