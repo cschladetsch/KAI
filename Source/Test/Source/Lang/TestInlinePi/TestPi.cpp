0d2dbe4d23c484c011a51fff07432fe37263c508
Christian <christian.schladetsch@gmail.com>
2017-02-25 00:30:31 +1100

    Un-commented Pi tests.

--- Source/Test/Source/Lang/TestInlinePi/TestPi.cpp
+++ Source/Test/Source/Lang/TestInlinePi/TestPi.cpp
@@ -10,21 +10,20 @@ using namespace std;
 struct TestPi : TestLangCommon
 {
 };
-/*
 
 TEST_F(TestPi, TestComments)
 {
-	_console->SetLanguage(Language::Pi);
-	_console->Execute("// text\n\n\n\n\n");
+	_console.SetLanguage(Language::Pi);
+	_console.Execute("// text\n\n\n\n\n");
 	ASSERT_EQ(_data->Size(), 0);
 
-	_console->Execute("// text\n");
+	_console.Execute("// text\n");
 	ASSERT_EQ(_data->Size(), 0);
 	
-	_console->Execute("// text");
+	_console.Execute("// text");
 	ASSERT_EQ(_data->Size(), 0);
 
-	_console->Execute("//");
+	_console.Execute("//");
 	ASSERT_EQ(_data->Size(), 0);
 }
 
@@ -32,46 +31,46 @@ TEST_F(TestPi, TestArithmetic)
 {
 	// TranslatorCommon::trace = 1;
 
-	_console->SetLanguage(Language::Pi);
+	_console.SetLanguage(Language::Pi);
 	_data->Clear();
-	_console->Execute("6 2 div");
+	_console.Execute("6 2 div");
 	ASSERT_EQ(AtData<int>(0), 3);
 
-	_console->Execute("1 2 +");
+	_console.Execute("1 2 +");
 	auto result = AtData<int>(0);
 	ASSERT_EQ(result, 3);
 
 	_data->Clear();
-	_console->Execute("1 2 -");
+	_console.Execute("1 2 -");
 	ASSERT_EQ(AtData<int>(0), -1);
 
 	_data->Clear();
-	_console->Execute("2 2 *");
+	_console.Execute("2 2 *");
 	ASSERT_EQ(AtData<int>(0), 4);
 
 	_data->Clear();
-	_console->Execute("3 2 + 2 2 * * 2 div");
+	_console.Execute("3 2 + 2 2 * * 2 div");
 	ASSERT_EQ(AtData<int>(0), 10);
 }
 
 TEST_F(TestPi, TestVectors)
 {
-	_console->SetLanguage(Language::Pi);
+	_console.SetLanguage(Language::Pi);
 
 	_data->Clear();
-	_console->Execute("1 1 toarray");
+	_console.Execute("1 1 toarray");
 	Pointer<Array> a = _data->Top();
 	ASSERT_EQ(a->Size(), 1);
 	ASSERT_EQ(ConstDeref<int>(a->At(0)), 1);
 
-	_console->Execute("[]");
+	_console.Execute("[]");
 	Pointer<Array> array = _data->At(0);
 	ASSERT_TRUE(array.Exists());
 	ASSERT_TRUE(array->Empty());
 	ASSERT_EQ(array->Size(), 0);
 
 	_data->Clear();
-	_console->Execute("[1 2 3]");
+	_console.Execute("[1 2 3]");
 	array = _data->At(0);
 	ASSERT_TRUE(array.Exists());
 	ASSERT_FALSE(array->Empty());
@@ -81,10 +80,9 @@ TEST_F(TestPi, TestVectors)
 	ASSERT_EQ(ConstDeref<int>(array->At(2)), 3);
 
 	_data->Clear();
-	_console->Execute("[1 2 3] size");
+	_console.Execute("[1 2 3] size");
 	ASSERT_EQ(AtData<int>(0), 3);
 }
-*/
 
 TEST_F(TestPi, TestScope)
 {
