15d7f7f1b917739d48c90e3b10f1f0e9bf8ea435
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-12-10 04:03:33 +1100

    Test freeze/thaw from Pi

--- Source/Test/Source/Lang/TestPi/TestPi.cpp
+++ Source/Test/Source/Lang/TestPi/TestPi.cpp
@@ -11,29 +11,18 @@ struct TestPi : TestLangCommon
 
 TEST_F(TestPi, TestComments)
 {
-    try
-    {
-        _console.SetLanguage(Language::Pi);
-        _console.Execute("// text\n\n\n\n\n");
-        ASSERT_EQ(_data->Size(), 0);
+    _console.SetLanguage(Language::Pi);
+    _console.Execute("// text\n\n\n\n\n");
+    ASSERT_EQ(_data->Size(), 0);
 
-        _console.Execute("// text\n");
-        ASSERT_EQ(_data->Size(), 0);
+    _console.Execute("// text\n");
+    ASSERT_EQ(_data->Size(), 0);
 
-        _console.Execute("// text");
-        ASSERT_EQ(_data->Size(), 0);
+    _console.Execute("// text");
+    ASSERT_EQ(_data->Size(), 0);
 
-        _console.Execute("//");
-        ASSERT_EQ(_data->Size(), 0);
-    }
-    catch (Exception::Base &e)
-    {
-        TEST_CERR << e.ToString().c_str();
-    }
-    catch (exception &e)
-    {
-        TEST_CERR << e.what();
-    }
+    _console.Execute("//");
+    ASSERT_EQ(_data->Size(), 0);
 }
 
 // Create some stuff on the stack, freeze it to a binary packet, thaw it out to objects,
@@ -42,10 +31,11 @@ TEST_F(TestPi, TestFreezeThaw)
 {
     _console.SetLanguage(Language::Pi);
     _console.Execute("42 \"hello\" [3 9 8] 3 toarray freeze");
-    TEST_COUT << _exec->PrintStack();
-    auto top = _data->Top();
-    ASSERT_TRUE(top.IsType<BinaryStream>());
+    auto const &packet = ConstDeref<BinaryStream>(_data->Top());
+    TEST_COUT << "Frozen to " << packet.Size() << " bytes";
     _console.Execute("thaw");
+
+    // the 'thaw' command will put all contents onto the stack
     auto stack = ConstDeref<Array>(_data->Top());
     ASSERT_EQ(3, stack.Size());
     auto a = stack.At(2);
@@ -64,7 +54,6 @@ TEST_F(TestPi, TestFreezeThaw)
     ASSERT_EQ(ConstDeref<int>(a1.At(0)), 3);
     ASSERT_EQ(ConstDeref<int>(a1.At(1)), 9);
     ASSERT_EQ(ConstDeref<int>(a1.At(2)), 8);
-
     ASSERT_EQ(b1, "hello");
     ASSERT_EQ(c1, 42);
 }
@@ -95,65 +84,51 @@ TEST_F(TestPi, TestArithmetic)
 
 TEST_F(TestPi, TestVectors)
 {
-    try
-    {
-        _console.SetLanguage(Language::Pi);
+    _console.SetLanguage(Language::Pi);
 
-        _data->Clear();
-        _console.Execute("1 1 toarray");
-        Pointer<Array> a = _data->Top();
-        ASSERT_EQ(a->Size(), 1);
-        ASSERT_EQ(ConstDeref<int>(a->At(0)), 1);
+    _data->Clear();
+    _console.Execute("1 1 toarray");
+    Pointer<Array> a = _data->Top();
+    ASSERT_EQ(a->Size(), 1);
+    ASSERT_EQ(ConstDeref<int>(a->At(0)), 1);
 
-        _console.Execute("[]");
-        Pointer<Array> array = _data->At(0);
-        ASSERT_TRUE(array.Exists());
-        ASSERT_TRUE(array->Empty());
-        ASSERT_EQ(array->Size(), 0);
+    _console.Execute("[]");
+    Pointer<Array> array = _data->At(0);
+    ASSERT_TRUE(array.Exists());
+    ASSERT_TRUE(array->Empty());
+    ASSERT_EQ(array->Size(), 0);
 
-        _data->Clear();
-        _console.Execute("[1 2 3]");
-        array = _data->At(0);
-        ASSERT_TRUE(array.Exists());
-        ASSERT_FALSE(array->Empty());
-        ASSERT_EQ(array->Size(), 3);
-        ASSERT_EQ(ConstDeref<int>(array->At(0)), 1);
-        ASSERT_EQ(ConstDeref<int>(array->At(1)), 2);
-        ASSERT_EQ(ConstDeref<int>(array->At(2)), 3);
+    _data->Clear();
+    _console.Execute("[1 2 3]");
+    array = _data->At(0);
+    ASSERT_TRUE(array.Exists());
+    ASSERT_FALSE(array->Empty());
+    ASSERT_EQ(array->Size(), 3);
+    ASSERT_EQ(ConstDeref<int>(array->At(0)), 1);
+    ASSERT_EQ(ConstDeref<int>(array->At(1)), 2);
+    ASSERT_EQ(ConstDeref<int>(array->At(2)), 3);
 
-        _data->Clear();
-        _console.Execute("[1 2 3] size");
-        ASSERT_EQ(AtData<int>(0), 3);
-    }
-    catch (Exception::Base &e)
-    {
-        TEST_CERR << e.ToString().c_str();
-    }
+    _data->Clear();
+    _console.Execute("[1 2 3] size");
+    ASSERT_EQ(AtData<int>(0), 3);
 }
 
 TEST_F(TestPi, TestScope)
 {
-    try
-    {
-        const Label b("b");
-        const Label c("c");
+    const Label b("b");
+    const Label c("c");
 
-        Pathname p("'/b");
-        EXPECT_TRUE(p.Validate());
+    Pathname p("'/b");
+    EXPECT_TRUE(p.Validate());
 
-        // store to explicit root of tree
-        _console.Execute("42 '/b #");
-        ASSERT_TRUE(_root.Has(b));
-        ASSERT_EQ(42, ConstDeref<int>(_root.Get(b)));
+    // store to explicit root of tree
+    _console.Execute("42 '/b #");
+    ASSERT_TRUE(_root.Has(b));
+    ASSERT_EQ(42, ConstDeref<int>(_root.Get(b)));
 
-        // this is only storing to local scope - within the context
-        // of the command-line in which it was executed - so it
-        // should not be preserved in the global tree
-        _console.Execute("1 'c #");
-        ASSERT_FALSE(_root.Has(c));
-    }
-    catch (Exception::Base &e)
-    {
-        ASSERT_TRUE(false) << e.ToString().c_str();
-    }
+    // this is only storing to local scope - within the context
+    // of the command-line in which it was executed - so it
+    // should not be preserved in the global tree
+    _console.Execute("1 'c #");
+    ASSERT_FALSE(_root.Has(c));
 }
