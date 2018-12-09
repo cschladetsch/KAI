38add77cdf0424dc29a43d1aef0b60c4ee4c4463
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-12-10 00:24:44 +1100

    Remove stale imgui embedded lib

--- Source/Test/Source/Lang/TestHlsl/TestHlsl.cpp
+++ Source/Test/Source/Lang/TestHlsl/TestHlsl.cpp
@@ -1,21 +1,21 @@
-#include "TestCommon.h"
-#include "KAI/Language/Hlsl/Hlsl.h"
-
-USING_NAMESPACE_KAI
-
-using namespace std;
-
-TEST(TestHlsl, Test)
-{
-    Console console;
-    Registry &reg = console.GetRegistry();
-    const char *text =( "float res = foo(pw*a) + bar(pw*123);");
-    auto lex = make_shared<HlslLexer>(text, reg);
-    lex->Process();
-    TEST_COUT << text << endl;
-    TEST_COUT << lex->Print() << endl;
-
-    HlslParser parse(reg);
-    parse.Process(lex, Structure::Statement);
-    TEST_COUT << parse.PrintTree() << endl;
-}
+//#include "TestCommon.h"
+//#include "KAI/Language/Hlsl/Hlsl.h"
+//
+//USING_NAMESPACE_KAI
+//
+//using namespace std;
+//
+//TEST(TestHlsl, Test)
+//{
+//    Console console;
+//    Registry &reg = console.GetRegistry();
+//    const char *text =( "float res = foo(pw*a) + bar(pw*123);");
+//    auto lex = make_shared<HlslLexer>(text, reg);
+//    lex->Process();
+//    TEST_COUT << text << endl;
+//    TEST_COUT << lex->Print() << endl;
+//
+//    HlslParser parse(reg);
+//    parse.Process(lex, Structure::Statement);
+//    TEST_COUT << parse.PrintTree() << endl;
+//}
